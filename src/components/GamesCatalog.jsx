36a71abe2eac8f00c951270dc88881fb408354f3
Alexis <alammedina46@gmail.com>
2026-01-19 18:52:21 -0300

    feat: add games catalog with search and category filter

--- src/components/GamesCatalog.jsx
+++ src/components/GamesCatalog.jsx
@@ -1,26 +1,53 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useContext } from "react";
 import GameCard from "./GameCard";
+import { GamesContext } from "../context/gamescontext.jsx";
 
-const GamesCatalog = ({ games }) => {
+const GamesCatalog = () => {
+    const { games } = useContext(GamesContext);
+
     const [search, setSearch] = useState("");
+    const [category, setCategory] = useState("all");
 
-    const filteredGames = games.filter((game) =>
-        game.title.toLowerCase().includes(search.toLowerCase())
-    );
+    const categories = [
+    "all",
+    ...new Set(games.map((game) => game.category)),
+    ];
+
+    const filteredGames = games.filter((game) => {
+        const matchTitle = game.title.toLowerCase().includes(search.toLowerCase());
+
+        const matchCategory =
+            category === "all" || game.category === category;
+
+        return matchTitle && matchCategory;
+    });
 
     return (
         <div className="container my-5">
             <div className="d-flex justify-content-between align-items-center mb-4">
                 <h3>Todos los juegos</h3>
 
+             <div className="d-flex gap-2 w-100 w-md-50">
                 <input 
                     type="text"
-                    className="form-control w-50"
+                    className="form-control"
                     placeholder="Buscar juegos..."
                     value={search}
-                    onChage={(e) => setSearch(e.target.value)}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
+
+                <select
+                    className="form-select w-25"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
+                   <option value="all">Todas</option>
+                   <option value="Terror / Acción">Terror / Acción</option>
+                   <option value="RPG / Aventura">RPG / Aventura</option>
+                   <option value="Acción / Aventura">Acción / Aventura</option>
+                   <option value="Deportes">Deportes</option>
+                </select>
+             </div>
             </div>
 
             <div className="row">
@@ -31,7 +58,7 @@ const GamesCatalog = ({ games }) => {
                         </div>
                     ))
                 ) : (
-                    <p>No se encontraron juegos.</p>
+                    <p className="text-muted">No se encontraron juegos.</p>
                 )}
             </div>
         </div>
