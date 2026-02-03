d20b290190ec2bb2e2ce756d147f90fc7cd70eab
Alexis <alammedina46@gmail.com>
2026-02-03 16:00:16 -0300

    feat: notfoundpage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "../styles/NotFound.css";
+import NotFoundImage from "../assets/notfoundimage.webp";
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>404 - Página No Encontrada</h1>
+    <div className="notfound-wrapper">
+      <div className="notfound-card">
+        <img src={NotFoundImage} alt="Página no encontrada" className="notfound-image" />
+        <div className="notfound-footer">
+          <Button variant="outline-lighty" size="sm" onClick={() => navigate(-1)}>
+            ← Volver atrás
+          </Button>
+
+          <Button variant="home-btn" size="sm" onClick={() => navigate("/")}>
+            Ir al inicio
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
